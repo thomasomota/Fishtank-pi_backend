17376354b8a9b244a95b7ed47694c6bce9202266
thomasomota <thomas.ulvoen@gmail.com>
2019-01-20 13:21:07 +0000

    commit so i can soft reset the pi

--- app.py
+++ app.py
@@ -12,13 +12,21 @@ p = GPIO.PWM(servo, 50)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return "Welcome to the fishfeeder API"
 
-@app.route("/feedfish")
+@app.route("/feedfish", methods=['POST'])
 def feed():
     print("feed function fired")
     feed_fish()
-    return "Fish is well fed"
+    return "Fish is fed!"
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
 def feed_fish():
 
@@ -42,5 +50,5 @@ def feed_fish():
         GPIO.cleanup()
 
 if __name__  == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
